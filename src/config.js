02fc867e1b44aa1dea0e30c745ab1464fffd3821
NathPaiva <hello@nathpaiva.com.br>
2017-08-22 22:12:59 -0300

    add auth

--- src/config.js
+++ src/config.js
@@ -1,3 +1,10 @@
+const TOKEN_API = 'BQAs_5LBZ77Qf9rcTAzDJgJE5JDIsunn7l-QSWtJi-rFfO4NzKyJG86CRH8_XeiZu7zPGOzInij2yYTidYbcNZ_Xr5-LLbAD434xe3TG1xSUgtmVQerLllzrFQ56y1yCsvJyWgJbvyam';
 const API_URL = 'https://api.spotify.com/v1';
 
-export default API_URL;
+const HEADERS = {
+  headers: {
+    Authorization: `'Bearer ${TOKEN_API}'`,
+  },
+};
+
+export { API_URL, HEADERS };
